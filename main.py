2cd271fac038bcd532dc017904a3368eba7562e2
and <67516292+salemalasmri@users.noreply.github.com>
2026-02-02 10:15:16 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
 import streamlit as st
 import requests
+import traceback
 
 st.set_page_config(page_title="EzOptions", layout="wide")
 
 url = "https://raw.githubusercontent.com/EazyDuz1t/EzOptions/main/ezoptions.py"
 
-response = requests.get(url)
-code = response.text
+try:
+    response = requests.get(url)
+    code = response.text
+    exec(code, globals())
 
-exec(code)
+except Exception:
+    st.error("Runtime Error:")
+    st.code(traceback.format_exc())
